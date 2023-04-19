ad3567b36a527724f83a318bd1292c3766ea172b
rakseng <mrakseng@gmail.com>
2023-04-19 22:14:14 +0600

    add favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Google Clone Next js 13',
   description: 'Google clone created by Next js 13',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({ children }) {
